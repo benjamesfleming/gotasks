91cb29faac5f03d17d12e0e775cbb78608e336b8
Ben Fleming <benfleming121@gmail.com>
2019-11-09 22:43:05 +1300

    change the navigate style to be more minimal

--- resources/scripts/components/Navigation.svelte
+++ resources/scripts/components/Navigation.svelte
@@ -3,38 +3,34 @@ import { IsAuthenticated, IsRegistered } from '~/utils/auth'
 </script>
 
 <style lang="postcss">
-nav {
-    background: theme('colors.primary');
-}
-
 nav > h2 {
     letter-spacing: .1rem;
 }
 
-nav > div {
-    height: 100%;
+nav a:not(:last-child) {
+    @apply mr-4;
 }
 </style>
 
-<nav class="flex justify-between text-white shadow-xl h-16">
+<nav class="container mx-auto flex justify-between text-gray-900 px-6 py-2">
 
-    <h2 class="font-bold px-4 w-32 flex items-center justify-center">
+    <h2 class="font-bold text-xl flex items-center justify-center">
         GoTasks
     </h2>
 
-    <div class="flex h-100">
+    <div class="flex h-full px-2">
     {#if !$IsAuthenticated}
     <a class="button" href="/auth/github/login?from=/#/auth-complete">
         <i class="fab fa-github"></i> Login
     </a>
     {:else}
         {#if $IsRegistered}
-        <a class="button" href="/#/app">Dashboard</a>
+        <a class="text-gray-900" href="/#/app">Dashboard</a>
         {:else}
-        <a class="button" href="/#/auth-complete">Complete Registation</a>
+        <a class="text-gray-900" href="/#/auth-complete">Complete Registation</a>
         {/if}
 
-        <a class="button" href="/#/auth-logout">Logout</a>
+        <a class="text-gray-900" href="/#/auth-logout">Logout</a>
     {/if}
     </div>
     
