6c93a3df443889a47d033f39094d91385ad050f5
Ben Fleming <benfleming121@gmail.com>
2019-11-09 22:39:56 +1300

    add more to custom tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: _t.colors.gray[500],
+        primary: _t.colors.gray[300],
+      },
+      opacity: {
+        '80': '0.8',
+        '90': '0.9'
+      },
+      maxWidth: {
+        ..._t.spacing,
       }
     }
   },
-  variants: {},
+  variants: {
+    backgroundColor: ['responsive', 'odd', 'hover', 'focus']
+  },
   plugins: [
     require('tailwindcss-transitions')(),
+    require('@tailwindcss/custom-forms'),
   ],
 }
