0c083638763ff205ed99803ec1d354876727c938
Ben Fleming <benfleming121@gmail.com>
2019-11-14 17:09:42 +1300

    rename auth object

--- resources/scripts/screens/AuthLogoutScreen.svelte
+++ resources/scripts/screens/AuthLogoutScreen.svelte
@@ -1,12 +1,12 @@
 <script>
 import { onMount } from 'svelte'
 import { navigateTo } from 'svero'
-import { UserObject } from '~/utils/auth'
+import { AuthObject } from '~/utils/auth'
 import { get } from '~/utils/api'
 
 onMount(async function () {
     await get('/auth/logout', {}, '').catch(console.error)
-    UserObject.set(null)
+    AuthObject.set(null)
     navigateTo('/#/')
 })
 </script>
