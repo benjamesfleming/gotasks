5bc1d68dabe1eee8961ac51477430b1fe889e128
Ben Fleming <benfleming121@gmail.com>
2019-11-14 16:49:20 +1300

    update moment locale to show shorthand time

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,7 +1,28 @@
+import * as moment from 'moment';
 import App from '~/App';
 import { reAuthenticate } from '~/utils/auth';
 
 reAuthenticate();
+
+moment.updateLocale('en', {
+    relativeTime: {
+      future : 'in %s',
+      past   : '%s ago',
+      s  : function (number, withoutSuffix) {
+        return withoutSuffix ? 'now' : 'a few seconds';
+      },
+      m  : '1m',
+      mm : '%dm',
+      h  : '1h',
+      hh : '%dh',
+      d  : '1d',
+      dd : '%dd',
+      M  : '1mth',
+      MM : '%dmth',
+      y  : '1y',
+      yy : '%dy'
+    }
+});
 
 const target = document.querySelector('#app');
 const app = new App(
