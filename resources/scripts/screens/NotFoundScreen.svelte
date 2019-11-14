6a3fc44fa38bc202d58736b5e064e08f4fcc9ae7
Ben Fleming <benfleming121@gmail.com>
2019-11-15 01:12:08 +1300

    style not found page

--- resources/scripts/screens/NotFoundScreen.svelte
+++ resources/scripts/screens/NotFoundScreen.svelte
@@ -1,1 +1,6 @@
-<h1>404 Not Found!</h1>
+<div style="height: 100vh" class="mx-auto py-12 flex items-center justify-center">
+    <img class="max-w-lg pr-12 drop-shadow-md hover:drop-shadow-lg transition-all" src="/assets/images/undraw_newspaper.svg" alt="Kiwi standing on oval">
+    <p class="text-5xl max-w-xl text-center font-extrabold text-gray-800">
+        WHO ARE YOU...<br/> YOU'RE NOT SUPPOSE TO BE HERE.<br/> TIME TO <a class="underline" href="#/">GO HOME</a>
+    </p>
+</div>
