8b0852a475cf9f5786ca2a0719825b7db83aed2f
Ben Fleming <benfleming121@gmail.com>
2019-11-14 22:54:41 +1300

    add create task handler

--- resources/scripts/store/_user.js
+++ resources/scripts/store/_user.js
@@ -38,7 +38,7 @@ export function createUserStore (user, { tasks }, saveToLocalStorage = false) {
         },
 
         // Toggle Task, toggle the task with the given id
-        // e.g. createUserStore(user).toggleTask(1)
+        // e.g. createUserStore(user).toggleTask(id)
         async toggleTask (id) {
             const { tasks } = await getStoreValue({ subscribe })
             const task = tasks.find(t => t.id == id)
@@ -47,6 +47,17 @@ export function createUserStore (user, { tasks }, saveToLocalStorage = false) {
                 await post(`/tasks/${task.id}`, task)
             }
             update(u => new User({ ...u, tasks }))
+        },
+
+        // Create Task, create a new task with given data
+        // e.g. createUserStore(user).createTask(task)
+        async createTask (task) {
+            const [response, error] = await post(`/tasks`, task)
+            const _t = Task.fromApi(response || {})
+            if (response != null) {
+                update(u => new User({ ...u, tasks: [...u.tasks, _t] }))
+            }
+            return [response != null, error]
         },
     }
 
