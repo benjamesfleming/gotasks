3d537c0b311bdf343af0d91ca309df4a9507b5a8
Ben Fleming <benfleming121@gmail.com>
2019-11-14 22:54:54 +1300

    add better error handling

--- resources/scripts/utils/api.js
+++ resources/scripts/utils/api.js
@@ -1,4 +1,5 @@
 import * as Cookie from 'js-cookie'
+import { camelCase } from 'lodash'
 
 /**
  * Post
@@ -49,7 +50,7 @@ export async function get(path, options={}, prefix='/api') {
  * @returns {array} [data, errors]
  */
 export async function __fetch(path, { headers, ...options }, prefix='') {
-    const response = await fetch(
+    let response = await fetch(
         prefix + path,
         { 
             headers: {
@@ -64,17 +65,17 @@ export async function __fetch(path, { headers, ...options }, prefix='') {
     switch (response.status) {
         // 401 Unauthorized
         case 401:
-            return [null, { code: 401, all: (await response.json()) }]
+            response = [null, { code: 401, all: (await response.json()) }]
             break
 
         // 400 Bad Request
         case 400:
-            return [null, { code: 400, all: (await response.json()) }]
+            response = [null, { code: 400, all: (await response.json()) }]
             break
 
         // 200 Success
         case 200:
-            return [(await response.json()), null]
+            response = [(await response.json()), null]
             break
 
         // Default
@@ -82,4 +83,12 @@ export async function __fetch(path, { headers, ...options }, prefix='') {
             throw new Error(`[GOTASKS] ERROR - Failed to fetch api \`${prefix}${path}\`. \n ${response.status} ${response.statusText}`)
             break
     }
+
+    if (response[1] != null) {
+        let errors = response[1].all || {}
+        response[1].all = Object.keys(errors)
+            .reduce((r,v) => r = ({ ...r, [camelCase(v)]: errors[v] }), {})
+    }
+
+    return response
 }
