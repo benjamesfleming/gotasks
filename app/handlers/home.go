1d6c2b8c9e24e499edec4c51a4efc011fbc3da4d
Ben Fleming <benfleming121@gmail.com>
2019-11-18 11:16:11 +1300

    switch to using html templates

--- app/handlers/home.go
+++ app/handlers/home.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"bytes"
+	"html/template"
+	"time"
+
 	rice "github.com/GeertJohan/go.rice"
 	"github.com/labstack/echo/v4"
 )
@@ -8,6 +12,15 @@ import (
 // HomeHandler is a default handler
 // GET /
 func HomeHandler(e echo.Context) error {
-	indexHTML, _ := e.Get("TemplatesBox").(*rice.Box).String("index.html")
-	return e.HTML(200, indexHTML)
+	t, _ := template.New("index").Parse(
+		e.Get("TemplatesBox").(*rice.Box).MustString("index.html"),
+	)
+
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, time.Now().Unix()); err != nil {
+		e.Logger().Fatalf("Failed To Prase HTML Template, %s", err)
+		return e.String(500, "Failed To Parse The HTML Template")
+	}
+
+	return e.HTML(200, tpl.String())
 }
