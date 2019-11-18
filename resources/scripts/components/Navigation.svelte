f2fac6ff10df895ce8e9ad9547039fec61029f2f
Ben Fleming <benfleming121@gmail.com>
2019-11-19 10:21:48 +1300

    set title to link home

--- resources/scripts/components/Navigation.svelte
+++ resources/scripts/components/Navigation.svelte
@@ -3,23 +3,19 @@ import { IsAuthenticated, IsRegistered } from '~/utils/auth'
 </script>
 
 <style lang="postcss">
-nav > h2 {
+nav > a {
     letter-spacing: .1rem;
-}
-
-nav a:not(:last-child) {
-    @apply mr-4;
 }
 </style>
 
 <nav class="container mx-auto flex justify-between text-gray-900 px-6 py-2">
 
-    <h2 class="font-bold text-xl flex items-center justify-center">
+    <a class="font-bold text-xl" href="/#/">
         GoTasks
-    </h2>
+    </a>
 
     <div class="flex h-full px-2">
-        <a class="text-gray-900" href="/#/app">Dashboard</a>
+        <a class="text-gray-900 mr-4" href="/#/app">Dashboard</a>
         <a class="text-gray-900" href="/#/auth-logout">Logout</a>
     </div>
     
