488e2b7cb72bb4adb3488381f26a3b56ed4cbed0
Ben Fleming <benfleming121@gmail.com>
2019-11-19 23:17:50 +1300

    move commands into own package

---
+++ app/commands/install.go
@@ -0,0 +1,131 @@
+package commands
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"os"
+
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/urfave/cli/v2"
+)
+
+// Random Hex
+// generate n hex bytes
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+// Extract File
+// extract any file from a given box to a given location, and templated with given data
+func extractFile(box *rice.Box, fn, dest string, data interface{}) {
+	log.Info().Msgf("Parsing Template [%s]", fn)
+	t, _ := template.New("file").Parse(
+		box.MustString(fn),
+	)
+
+	log.Info().Msgf("Executing Template [%s]", fn)
+	tpl := new(bytes.Buffer)
+	if err := t.Execute(tpl, data); err != nil {
+		log.Error().Msgf("Failed To Execute Template [%s]", fn)
+		log.Fatal().Msg("Aborting")
+		panic(err)
+	}
+
+	log.Info().Msgf("Writing Template [%s] File To Disk", fn)
+	if err := ioutil.WriteFile(dest+"/"+fn, tpl.Bytes(), 0644); err != nil {
+		log.Error().Msgf("Failed To Save Template [%s] To Disk", fn)
+		log.Fatal().Msg("Aborting")
+		panic(err)
+	}
+
+	log.Info().Msgf("  -> %s", dest+"/"+fn)
+}
+
+// InstallCommand ...
+var InstallCommand = &cli.Command{
+	Name:  "install",
+	Usage: "add supporting files",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "with-sqlite3",
+			Usage: "Generate a blank sqlite3 database",
+		},
+		&cli.StringFlag{
+			Name:     "output",
+			Aliases:  []string{"o"},
+			Usage:    "Save configuration template to `DIR`",
+			Required: true,
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+
+		fmt.Println("----")
+
+		// Checking Given Arguments
+		// check that the users arguments are valid
+		dest := ctx.String("output")
+		if stat, err := os.Stat(dest); err != nil || !stat.IsDir() {
+			if os.IsNotExist(err) || !stat.IsDir() {
+				log.Error().Msgf("Failed To Access Output [%s], Ensure Directory Exists", dest)
+			} else {
+				log.Error().Err(err).Msg("Failed To Access Output Directory")
+			}
+			log.Fatal().Msg("Aborting")
+			panic(err)
+		}
+
+		// Generating Unique Key
+		// this gerneates the unique key to load into the template
+		log.Info().Msg("Installing Supporting Files")
+		log.Info().Msg("Generating Unique Key")
+		key, err := randomHex(32)
+		if err != nil {
+			log.Error().Msg("Failed To Genereate A Unique Key")
+			log.Fatal().Msg("Aborting")
+			panic(err)
+		}
+		log.Info().Msgf("  -> %s", key)
+
+		// Load Templates Box
+		// this finds the config template on either the file system or the embedded resources
+		log.Info().Msg("Loading Templates Box")
+		box, err := rice.FindBox("../../templates")
+		if err != nil {
+			log.Error().Msg("Failed To Load The Templates Box")
+			log.Fatal().Msg("Aborting")
+			panic(err)
+		}
+
+		// Extract Files
+		// extract all the files to their correct install locations
+		extractFile(box, "gotasks.sample.toml", ctx.String("output"), key)
+
+		// Create Database File
+		// create a blank sqlite3 database
+		if ctx.Bool("with-sqlite3") {
+			log.Info().Msg("Creating a Blank Database File")
+			if _, err := os.Create(dest + "/database.db"); err != nil {
+				log.Error().Msg("Failed To Create The Database")
+				log.Fatal().Msg("Aborting")
+				panic(err)
+			}
+			log.Info().Msgf("  -> %s", dest+"/database.db")
+		} else {
+			log.Warn().Msg("Skipping Database Creation, Use --with-sqlite3 To Generate Database")
+		}
+
+		log.Info().Msg("Done")
+		return nil
+	},
+}
--- main.go
+++ main.go
@@ -1,115 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/signal"
 	"sort"
 
-	"git.benfleming.nz/benfleming/gotasks/app"
+	. "git.benfleming.nz/benfleming/gotasks/app/commands"
 	"github.com/urfave/cli/v2"
-	"github.com/urfave/cli/v2/altsrc"
 )
 
-func or(ctx *cli.Context, a, b string) string {
-	if ctx.IsSet(a) {
-		return ctx.String(a)
-	}
-	return b
-}
-
 func main() {
-	cliFlags := []cli.Flag{
-		// Server Config
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "server.port", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "server.bind", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "server.host", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "server.avatar-dir", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "server.unique-key", Hidden: true}),
-
-		// Database Config
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "database.type", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "database.connection", Hidden: true}),
-
-		// Github Config
-		altsrc.NewBoolFlag(&cli.BoolFlag{Name: "auth.github.enabled", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "auth.github.client-id", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "auth.github.secret", Hidden: true}),
-
-		// Google Config
-		altsrc.NewBoolFlag(&cli.BoolFlag{Name: "auth.google.enabled", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "auth.google.client-id", Hidden: true}),
-		altsrc.NewStringFlag(&cli.StringFlag{Name: "auth.google.secret", Hidden: true}),
-
-		// Global Config
-		&cli.StringFlag{
-			Name:     "config",
-			Aliases:  []string{"c"},
-			Usage:    "Load configuration from `FILE`",
-			Required: true,
-		},
-	}
-
 	cliApp := &cli.App{
-		Flags:       cliFlags,
 		Name:        "gotasks",
 		Usage:       "get stuff done",
 		Description: "A Task Tracker & Todo List",
 		HideVersion: true,
+		Flags:       []cli.Flag{},
 		Commands: []*cli.Command{
-			{
-				Name:  "init",
-				Usage: "add empty config files",
-				Action: func(ctx *cli.Context) error {
-					return nil
-				},
-			},
-			{
-				Name:   "start",
-				Usage:  "start the server",
-				Before: altsrc.InitInputSourceWithContext(cliFlags, altsrc.NewTomlSourceFromFlagFunc("config")),
-				Action: func(ctx *cli.Context) error {
-					opts := &app.ServerOptions{
-						// Server Config
-						HTTPPort:  or(ctx, "server.port", "3000"),
-						BindAddr:  or(ctx, "server.bind", "0.0.0.0"),
-						Host:      or(ctx, "server.host", "https://127.0.0.1:3000"),
-						AvatarDir: or(ctx, "server.avatar-dir", "/tmp/gotasks/avatars"),
-						UniqueKey: ctx.String("server.unique-key"),
-
-						// Database Config
-						DatabaseType:       or(ctx, "database.type", "sqlite3"),
-						DatabaseConnection: or(ctx, "database.connection", "/tmp/gotasks/database.db"),
-
-						// Github Config
-						GithubEnabled:  ctx.Bool("auth.github.enabled"),
-						GithubClientID: ctx.String("auth.github.client-id"),
-						GithubSecret:   ctx.String("auth.github.secret"),
-
-						// Google Config
-						GoogleEnabled:  ctx.Bool("auth.google.enabled"),
-						GoogleClientID: ctx.String("auth.google.client-id"),
-						GoogleSecret:   ctx.String("auth.google.secret"),
-					}
-
-					s := app.NewServer(opts)
-					c := make(chan os.Signal, 1)
-
-					signal.Notify(c, os.Interrupt)
-					go func() {
-						<-c
-
-						fmt.Println("----")
-						s.Logger.Infof("Shutdown Signal Recived, Goodbye")
-
-						s.Router.Close()
-						s.DB.Close()
-					}()
-
-					return s.Start()
-				},
-			},
+			InstallCommand, StartCommand,
 		},
 	}
 
