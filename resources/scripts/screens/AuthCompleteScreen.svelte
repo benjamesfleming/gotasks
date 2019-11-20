e3657629e1f8eb609debf544c631355696eab956
Ben Fleming <benfleming121@gmail.com>
2019-11-20 13:21:51 +1300

    dont show anything while loading

--- resources/scripts/screens/AuthCompleteScreen.svelte
+++ resources/scripts/screens/AuthCompleteScreen.svelte
@@ -62,59 +62,56 @@ onMount(async function () {
 })
 </script>
 
-<Modal fadeIn={false}>
-{#if isLoading == true}
-    <p>Loading...</p>  
-{:else}
-    
-    <img style="margin-top: -120px;" class="w-1/2 mx-auto mb-6 rounded-lg shadow-md hover:shadow-lg transition-all" src={avatar} alt=""/>
+{#if isLoading == false}
+    <Modal fadeIn={false}>
+        <img style="margin-top: -120px;" class="w-1/2 mx-auto mb-6 rounded-lg shadow-md hover:shadow-lg transition-all" src={avatar} alt=""/>
 
-    <!-- 
-        Modal Header
-     -->
-    <div class="flex flex-col text-gray-800 p-2 mb-6">
-        <h2 class="text-xl font-extrabold">COMPLETE REGISTRATION</h2>
-        <span>Enter details to finish registration</span>
-    </div>
-
-    <div class="flex">
         <!-- 
-            First Name Input
+            Modal Header
+        -->
+        <div class="flex flex-col text-gray-800 p-2 mb-6">
+            <h2 class="text-xl font-extrabold">COMPLETE REGISTRATION</h2>
+            <span>Enter details to finish registration</span>
+        </div>
+
+        <div class="flex">
+            <!-- 
+                First Name Input
+                -->
+            <label class="w-1/2 flex flex-col justify-between px-2 pb-2 bg-white overflow-hidden">
+                <span class="text-gray-700">First Name</span>
+                {#if errors.firstName}<span class="block text-sm text-red-500">*{errors.firstName}</span>{/if}
+                <input class="form-input mt-1 block w-full {errors.firstName ? 'border-red-500' : ''}" placeholder="" bind:value={firstName} on:focus={() => onClearError('firstName')}>
+            </label>
+
+            <!-- 
+                Last Name Input
+                -->
+            <label class="w-1/2 flex flex-col justify-between px-2 pb-2 bg-white overflow-hidden">
+                <span class="text-gray-700">Last Name</span>
+                {#if errors.lastName}<span class="block text-sm text-red-500">*{errors.lastName}</span>{/if}
+                <input class="form-input mt-1 block w-full {errors.lastName ? 'border-red-500' : ''}" placeholder="" bind:value={lastName} on:focus={() => onClearError('lastName')}>
+            </label>
+        </div>
+
+        <!-- 
+            Username Input
             -->
-        <label class="w-1/2 flex flex-col justify-between px-2 pb-2 bg-white overflow-hidden">
-            <span class="text-gray-700">First Name</span>
-            {#if errors.firstName}<span class="block text-sm text-red-500">*{errors.firstName}</span>{/if}
-            <input class="form-input mt-1 block w-full {errors.firstName ? 'border-red-500' : ''}" placeholder="" bind:value={firstName} on:focus={() => onClearError('firstName')}>
+        <label class="block px-2 pb-2 bg-white overflow-hidden">
+            <span class="text-gray-700">Username</span>
+            {#if errors.username}<span class="block text-sm text-red-500">*{errors.username}</span>{/if}
+            <input class="form-input mt-1 block w-full {errors.username ? 'border-red-500' : ''}" placeholder="" bind:value={username} on:focus={() => onClearError('username')}>
         </label>
 
         <!-- 
-            Last Name Input
+            Email Input
             -->
-        <label class="w-1/2 flex flex-col justify-between px-2 pb-2 bg-white overflow-hidden">
-            <span class="text-gray-700">Last Name</span>
-            {#if errors.lastName}<span class="block text-sm text-red-500">*{errors.lastName}</span>{/if}
-            <input class="form-input mt-1 block w-full {errors.lastName ? 'border-red-500' : ''}" placeholder="" bind:value={lastName} on:focus={() => onClearError('lastName')}>
+        <label class="block px-2 pb-2 bg-white overflow-hidden">
+            <span class="text-gray-700">Email</span>
+            {#if errors.email}<span class="block text-sm text-red-500">*{errors.email}</span>{/if}
+            <input class="form-input mt-1 block w-full {errors.email ? 'border-red-500' : ''}" type="email" placeholder="" bind:value={email} on:focus={() => onClearError('email')}>
         </label>
-    </div>
 
-    <!-- 
-        Username Input
-        -->
-    <label class="block px-2 pb-2 bg-white overflow-hidden">
-        <span class="text-gray-700">Username</span>
-        {#if errors.username}<span class="block text-sm text-red-500">*{errors.username}</span>{/if}
-        <input class="form-input mt-1 block w-full {errors.username ? 'border-red-500' : ''}" placeholder="" bind:value={username} on:focus={() => onClearError('username')}>
-    </label>
-
-    <!-- 
-        Email Input
-        -->
-    <label class="block px-2 pb-2 bg-white overflow-hidden">
-        <span class="text-gray-700">Email</span>
-        {#if errors.email}<span class="block text-sm text-red-500">*{errors.email}</span>{/if}
-        <input class="form-input mt-1 block w-full {errors.email ? 'border-red-500' : ''}" type="email" placeholder="" bind:value={email} on:focus={() => onClearError('email')}>
-    </label>
-
-    <button class="form-button m-2 mt-0" on:click={onContinueClick}>Register</button>
+        <button class="form-button m-2 mt-0" on:click={onContinueClick}>Register</button>
+    </Modal>
 {/if}
-</Modal>
