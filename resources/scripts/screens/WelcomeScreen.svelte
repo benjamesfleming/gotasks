9cac0a19a180c94391a702ae0e3b95775ab22805
Ben Fleming <benfleming121@gmail.com>
2019-12-05 10:53:22 +1300

    style to mobile first

--- resources/scripts/screens/WelcomeScreen.svelte
+++ resources/scripts/screens/WelcomeScreen.svelte
@@ -1,28 +1,37 @@
 <script>
+import { onMount } from 'svelte'
+import fitty from 'fitty'
 import { IsAuthenticated, IsRegistered } from '~/utils/auth'
+
+onMount(() => {
+    fitty(document.querySelector('#title'))
+    fitty(document.querySelector('#subtitle'))
+})
 </script>
 
 <style lang="postcss">
 @import "../../styles/components/bg-animate";
 
-.container {
+#container {
     height: 100vh;
+    width: 768px;
+    max-width: 90%;
     display: flex;
     align-items: center;
     justify-content: center;
 }
 
-.subtitle,
-.title,
-.signin-btn {
+#subtitle,
+#title,
+#signin-btn {
     color: theme('colors.gray.800');
     font-weight: 900;
     letter-spacing: 0.1em;
 }
 
-.title:hover + .subtitle,
-.title:hover,
-.signin-btn:hover {
+#title:hover + #subtitle,
+#title:hover,
+#signin-btn:hover {
     @apply bg-animate-short;
     @apply bg-animate-alternate;
     @apply bg-animate-colorless;
@@ -30,49 +39,51 @@ import { IsAuthenticated, IsRegistered } from '~/utils/auth'
     cursor: pointer;
 }
 
-.title:hover + .subtitle,
-.title:hover {
+#title:hover + #subtitle,
+#title:hover {
     background: none;
 }
 
-.title {
-    font-size: 12vw;
-    text-shadow: 5px 5px 0 theme('colors.highlight');
-    padding-bottom: 1.5rem;
+#title {
+    font-size: 4em;
+    text-shadow: 0.05em 0.05em 0 theme('colors.highlight');
+    padding-bottom: 0;
 }
 
-.subtitle {
-    font-size: 3vw;
-    text-shadow: 2px 2px 0 theme('colors.highlight');
-    margin-top: -75px;
+#subtitle {
+    font-size: 1.25em;
+    letter-spacing: 0.2em;
+    text-shadow: 0.075em 0.075em 0 theme('colors.highlight');
+    margin-top: -0.5em;
+    padding: 0 5%;
 }
 
-.signin-btn {
-    padding: 1rem 3rem;
-    font-size: 1.5vw;
+#signin-btn {
+    padding: 0.75em 2em;
+    font-size: 1em;
     width: fit-content;
-    margin: 3rem auto;
+    margin: 1em auto;
     border: 5px solid theme('colors.highlight');
 }
 </style>
 
-<div class="container flex-col mx-auto">
+<div id="container" class="flex-col mx-auto">
 
-    <h1 class="title w-full text-center">GoTasks</h1>
-    <h2 class="subtitle w-full text-center text-4xl pointer-events-none">GET STUFF DONE</h2>
+    <span id="title" class="pointer-events-none">GoTasks</span>
+    <span id="subtitle" class="pointer-events-none">GET STUFF DONE</span>
 
     {#if !$IsAuthenticated}
-    <a class="signin-btn" href="/auth/github/login?from=/%23/auth-complete">
+    <a id="signin-btn" href="/auth/github/login?from=/%23/auth-complete">
         <i class="fab fa-github mr-3" />
         <span class="mr-2">SIGN IN</span>
     </a>
     {:else}
         {#if !$IsRegistered}
-        <a class="signin-btn" href="/#/auth-complete">
+        <a id="signin-btn" href="/#/auth-complete">
             COMPLETE REGISTATION
         </a>
         {:else}
-        <a class="signin-btn" href="/#/app">
+        <a id="signin-btn" href="/#/app">
             OPEN DASHBOARD
         </a>
         {/if}
