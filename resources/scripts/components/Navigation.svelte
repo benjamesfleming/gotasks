c5d79ffb896bcc94dca146c39437e41a293616c5
Ben Fleming <benfleming121@gmail.com>
2019-12-05 10:41:57 +1300

    navigation styling to mobile first design

--- resources/scripts/components/Navigation.svelte
+++ resources/scripts/components/Navigation.svelte
@@ -1,24 +1,32 @@
 <script>
 import { IsAuthenticated, IsRegistered } from '~/utils/auth'
+
+let open = false
 </script>
 
 <style lang="postcss">
 nav > a {
     letter-spacing: .1rem;
 }
+
+.menu-icon {
+    top: 0;
+    right: 0;
+}
 </style>
 
-<nav class="container mx-auto flex justify-between text-gray-900 px-6 py-2">
+<nav class="container m-auto relative flex flex-col sm:flex-row justify-between text-gray-900 py-2 px-4 sm:px-6">
 
     <a class="font-bold text-xl" href="/#/">
         GoTasks
     </a>
 
-    <div class="flex h-full px-2">
-        <a class="text-gray-900 mr-4" href="/#/app">Dashboard</a>
-        <a class="text-gray-900 mr-4" href="/#/help">Help</a>
-        <a class="text-gray-900 mr-4" href="/#/account">Account</a>
-        <a class="text-gray-900" href="/#/auth-logout">Logout</a>
+    <div class="flex flex-col sm:flex-row h-full p-2 sm:pt-0">
+        <i class="cursor-pointer menu-icon fas fa-bars absolute m-4 sm:hidden" on:click={() => open = !open}></i>
+        <a class="cursor-pointer text-gray-900 sm:mr-4 {!open && 'hidden sm:inline-block'}" href="/#/app">Dashboard</a>
+        <a class="cursor-pointer text-gray-900 sm:mr-4 {!open && 'hidden sm:inline-block'}" href="/#/help">Help</a>
+        <a class="cursor-pointer text-gray-900 sm:mr-4 {!open && 'hidden sm:inline-block'}" href="/#/account">Account</a>
+        <a class="cursor-pointer text-gray-900 {!open && 'hidden sm:inline-block'}" href="/#/auth-logout">Logout</a>
     </div>
     
 </nav>
